5d629eada11590f699a57d4758a6a2169e239636
Robert Sotomski <sotomski@gmail.com>
2015-10-20 22:56:32 +0200

    Improved grayscale conversion performance

--- src/main/java/pl/sotomski/apoz/utils/ImageUtils.java
+++ src/main/java/pl/sotomski/apoz/utils/ImageUtils.java
@@ -2,40 +2,36 @@ package pl.sotomski.apoz.utils;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
+import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 
 /**
  * Created by sotomski on 01/10/15.
  */
 public class ImageUtils {
+    private static final double rPerc = 0.2;
+    private static final double gPerc = 0.6;
+    private static final double bPerc = 0.2;
 
     public static BufferedImage rgbToGrayscale(BufferedImage bufferedImage){
+        int channels = bufferedImage.getColorModel().getNumComponents();
+        if (channels==1) return bufferedImage;
+
         BufferedImage grayscaleImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
-        WritableRaster wr = grayscaleImage.getRaster();
         int width = bufferedImage.getWidth();
         int height = bufferedImage.getHeight();
-        for(int x=0;x<width;++x) {
-            for(int y=0;y<height;++y) {
-                int rgb = bufferedImage.getRGB(x,y);
-                wr.setSample(x, y, 0, convertPixelToGray(rgb));
-            }
+
+        final byte[] a = ((DataBufferByte) bufferedImage.getRaster().getDataBuffer()).getData();
+        final byte[] gray = ((DataBufferByte) grayscaleImage.getRaster().getDataBuffer()).getData();
+        for (int p = 0; p < width * height * channels; p += channels) {
+
+            int r = a[p+2] & 0xFF;
+            int g = a[p+1] & 0xFF;
+            int b = a[p] & 0xFF;
+            gray[p/channels] = (byte) ((r * rPerc) + (g * gPerc) + (b * bPerc));
+
         }
         return grayscaleImage;
-    }
-
-    private static int convertPixelToGray(int rgb) {
-        //TODO Add LUT
-        /*
-        rPerc+gPerc+bPerc == 1
-         */
-        final double rPerc = 0.2;
-        final double gPerc = 0.6;
-        final double bPerc = 0.2;
-        int r = (rgb >> 16) & 0xFF;
-        int g = (rgb >> 8) & 0xFF;
-        int b = rgb & 0xFF;
-        int k = (int)(r*rPerc + g*gPerc + b*bPerc) ;
-        return k;
     }
 
     public static int getR(int rgb) {
